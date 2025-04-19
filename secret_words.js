83116a57a6e02735984fc53ab5216dad4dfdbac4
Tessiland <giovanni@tessiland.com>
2025-04-19 12:15:39 +0200

    Add files via upload

---
+++ secret_words.js
@@ -0,0 +1,8 @@
+const secretWords = [
+  "gomitolo rosa", "uncinetto magico", "punto basso segreto", "granny misterioso", "borsa stellare",
+  "filato arcobaleno", "copertina fatata", "Tessiluxe dorato", "costume incantato", "cordino segreto",
+  "donny d’estate", "juta country", "bag crochet", "filo brillante", "schema magico",
+  "punto invisibile", "square nascosto", "catena dorata", "uncinetto invisibile", "pizzo crochet",
+  "punti nascosti", "filato misterioso", "cuore di lana", "intreccio fatato", "trama perfetta",
+  "loop incantato", "filo svelato", "punto gioiello", "gomitolo nascosto", "maglia segreta"
+];
